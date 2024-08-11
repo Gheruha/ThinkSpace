eb9a436e8fc30893bb6d67a05f68706a796bf35b
Curnic Dorin <157069618+C-Dorin@users.noreply.github.com>
2024-08-11 18:45:49 +0300

    Restructured the project and added icon

--- gulpfile.ts
+++ gulpfile.ts
@@ -3,14 +3,14 @@ const sass = require('gulp-sass')(require('sass'));
 const purgecss = require('gulp-purgecss');
 
 function buildStyles() {
-	return src('app/styles/scss/**/*.scss')
+	return src('src/styles/scss/**/*.scss')
 		.pipe(sass().on('error', sass.logError))
-		.pipe(purgecss({ content: ['./**/*.tsx'] }))
-		.pipe(dest('app/styles/css'));
+		.pipe(purgecss({ content: ['src/**/*.tsx'] }))
+		.pipe(dest('src/styles/css'));
 }
 
 function watchTask() {
-	watch(['app/styles/scss/**/*.scss', './**/*.tsx'], buildStyles);
+	watch(['src/styles/scss/**/*.scss', 'src/**/*.tsx'], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import { GeistSans } from 'geist/font/sans';
-import './styles/css/global.css';
+import '@/styles/css/global.css';
 
 const defaultUrl = process.env.VERCEL_URL
 	? `https://${process.env.VERCEL_URL}`
@@ -7,13 +7,19 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
 	metadataBase: new URL(defaultUrl),
-	title: 'Next.js and Supabase Starter Kit',
-	description: 'The fastest way to build apps with Next.js and Supabase'
+	title: 'Think Space',
+	description: 'Organize your life quickly and easily'
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en" className={GeistSans.className}>
+			<head>
+				<link rel="apple-touch-icon" sizes="180x180" href="/favicon_io/apple-touch-icon.png" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon_io/favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicon_io/favicon-16x16.png" />
+				<link rel="manifest" href="/site.webmanifest" />
+			</head>
 			<body className="bg-background text-foreground">
 				<main className="min-h-screen flex flex-col items-center">{children}</main>
 			</body>
