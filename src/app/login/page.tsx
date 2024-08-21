aa7181bf29414df02587987034bec0d6a0625257
Gheruha <maxgheruha@gmail.com>
2024-08-21 13:24:14 +0300

    Changed login form design

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -58,13 +58,13 @@ export default function Login() {
 	};
 
 	return (
-		<div className="flex flex-col w-full items-center gap-2 ">
-			<form className="flex flex-col w-2/4 text-foreground">
+		<div className="flex flex-col w-full h-[100vh] justify-center border items-center gap-2 p-10">
+			<form className="login">
 				<label className="text-md" htmlFor="email">
 					Email
 				</label>
 				<input
-					className="rounded-md px-4 py-2 bg-inherit border mb-6"
+					className="login-input-text"
 					name="email"
 					placeholder="you@example.com"
 					required
@@ -75,7 +75,7 @@ export default function Login() {
 					Password
 				</label>
 				<input
-					className="rounded-md px-4 py-2 bg-inherit border mb-6"
+					className="login-input-text"
 					type="password"
 					name="password"
 					placeholder="••••••••"
@@ -84,13 +84,13 @@ export default function Login() {
 					onChange={(e) => setData((prevData) => ({ ...prevData, password: e.target.value }))}
 				/>
 				<button
-					className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
+					className="border bg-violet-700 text-white border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
 					onClick={(e) => signUp(e)}
 				>
 					Sign Up
 				</button>
 				<button
-					className="border bg-violet-700 text-white border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
+					className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
 					onClick={(e) => signIn(e)}
 				>
 					Sign In
