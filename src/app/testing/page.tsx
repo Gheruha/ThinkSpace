6757a32f935bbf0a385e5782e32f91c767f2e07b
Curnic Dorin <157069618+C-Dorin@users.noreply.github.com>
2024-08-26 19:45:27 +0300

    Made the menu with NavigationMenu and adapted for mobiles

--- src/app/testing/page.tsx
+++ src/app/testing/page.tsx
@@ -1,10 +1,10 @@
 'use client';
 
-export default function Pricing() {
+export default function Testing() {
 	return (
-		<div className="flex items-center h-[60vh]">
+		<div className="flex flex-col items-center h-menu-product space-y-10">
 			<p className="text-8xl border-4 text-blue-900 border-blue-800 rounded-3xl p-4 dark:text-blue-400 dark:border-blue-400">
-				Coming soon
+				Testing page
 			</p>
 		</div>
 	);
