70a29ae7da580a9b3eb51eec817385c367446343
Curnic Dorin <157069618+C-Dorin@users.noreply.github.com>
2024-08-28 09:55:53 +0300

    Added some UI to Pricing page

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,11 +1,104 @@
 'use client';
 
+import { useState } from 'react';
+import { Button } from '@/components/ui/button';
+import { Card, CardTitle } from '@/components/ui/card';
+import { Label } from '@/components/ui/label';
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
+
 export default function Pricing() {
+	const [isMonthly, setIsMonthly] = useState(true);
 	return (
-		<div className="flex items-center h-[60vh]">
-			<p className="text-8xl border-4 text-blue-900 border-blue-800 rounded-3xl p-4 dark:text-blue-400 dark:border-blue-400">
-				Coming soon
-			</p>
+		<div className="flex flex-col items-center">
+			<p className="hidden lg:block text-6xl font-semibold">Your personal toolkit.</p>
+			<p className="hidden lg:block text-5xl font-semibold">Start free and empower your journey.</p>
+			<p className="lg:hidden text-6xl font-semibold">Pricing</p>
+			<Tabs defaultValue="monthly" className="pricing-tab">
+				<TabsList className="grid w-full grid-cols-2 rounded-3xl">
+					<TabsTrigger
+						value="monthly"
+						className="rounded-3xl"
+						onClick={() => {
+							setIsMonthly(true);
+						}}
+					>
+						Pay monthly
+					</TabsTrigger>
+					<TabsTrigger
+						value="yearly"
+						className="rounded-3xl space-x-1.5"
+						onClick={() => {
+							setIsMonthly(false);
+						}}
+					>
+						<p>Pay yearly</p>
+						<p className="text-xs font-normal" style={{ color: 'hsl(var(--primary))' }}>
+							save 30%
+						</p>
+					</TabsTrigger>
+				</TabsList>
+			</Tabs>
+			<div className="grid grid-cols-12 auto-rows-min gap-x-6 w-full">
+				<div className="pricing-plans-grid">
+					<Card className="flex flex-col flex-grow self-end h-full p-[28px_32px_24px]">
+						<Label>Free</Label>
+						<CardTitle className="pb-6 pt-2">$0</CardTitle>
+						<Button variant="outline">Get started</Button>
+						<ul
+							className="pt-4 pl-4 text-gray-700 dark:text-gray-300"
+							style={{ listStyleType: 'disc' }}
+						>
+							<li>Collaborative workspace</li>
+							<li>Integrate with Slack, GitHub & more</li>
+							<li>Basic page analytics</li>
+							<li>7 day page history</li>
+							<li>Invite 10 guests</li>
+						</ul>
+					</Card>
+				</div>
+				<div className="pricing-plans-grid">
+					<Card className="flex flex-col flex-grow self-end h-full p-[28px_32px_24px]">
+						<Label>Pro</Label>
+						{isMonthly ? (
+							<CardTitle className="pb-6 pt-2">$12</CardTitle>
+						) : (
+							<CardTitle className="pb-6 pt-2">$8</CardTitle>
+						)}
+						<Button variant="outline">Get started</Button>
+						<ul
+							className="pt-4 pl-4 text-gray-700 dark:text-gray-300"
+							style={{ listStyleType: 'disc' }}
+						>
+							<li>Collaborative workspace</li>
+							<li>Integrate with Slack, GitHub & more</li>
+							<li>Basic page analytics</li>
+							<li>7 day page history</li>
+							<li>Invite 10 guests</li>
+						</ul>
+					</Card>
+				</div>
+				<div className="pricing-plans-grid">
+					<Card className="flex flex-col flex-grow self-end h-full p-[28px_32px_24px]">
+						<Label>King</Label>
+						{isMonthly ? (
+							<CardTitle className="pb-6 pt-2">$50</CardTitle>
+						) : (
+							<CardTitle className="pb-6 pt-2">$35</CardTitle>
+						)}
+						<Button variant="outline">Get started</Button>
+						<ul
+							className="pt-4 pl-4 text-gray-700 dark:text-gray-300"
+							style={{ listStyleType: 'disc' }}
+						>
+							<li>Collaborative workspace</li>
+							<li>Integrate with Slack, GitHub & more</li>
+							<li>Basic page analytics</li>
+							<li>7 day page history</li>
+							<li>Invite 10 guests</li>
+						</ul>
+					</Card>
+				</div>
+			</div>
 		</div>
 	);
 }
