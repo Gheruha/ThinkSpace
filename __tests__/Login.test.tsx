27617392c037f3c2ddbe001b9341ee158f769863
Gheruha <maxgheruha@gmail.com>
2024-09-16 17:15:34 +0300

    Checking the render of auth page

---
+++ __tests__/Login.test.tsx
@@ -0,0 +1,28 @@
+import { render } from '@testing-library/react';
+import { ButtonProvider } from '@/app/product/components/clickedButton';
+import { LoginForm } from '@/app/auth/components/forms/log_in';
+import { useRouter } from 'next/navigation';
+
+// Mocking the useRouter
+jest.mock('next/navigation', () => ({
+	useRouter: jest.fn()
+}));
+
+it('should show the Auth page', () => {
+	// Simulating the useRouter in testing environment
+	const routerPush = jest.fn();
+
+	(useRouter as jest.Mock).mockReturnValue({
+		push: routerPush
+	});
+
+	// Virtual DOM
+	render(
+		<ButtonProvider>
+			<LoginForm />
+		</ButtonProvider>
+	);
+
+	// Expectation
+	// ...
+});
--- __tests__/Product.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from '@testing-library/react';
-import Product from '@/app/product/page';
-
-it('should load Product page', () => {
-	const { container } = render(<Product />);
-	expect(container).toMatchSnapshot();
-});
