9275e46af1cc1ea5da29ba85d36a65aa512c86ff
Gheruha <maxgheruha@gmail.com>
2025-02-06 14:13:33 +0200

    Add workspace-related files

--- src/app/workspace/[pageID]/page.tsx
+++ src/app/workspace/[pageID]/page.tsx
@@ -1,9 +1,7 @@
-import { PageProps } from '@/lib/dto/workspace/workspace.dto';
-const Page = async ({ params }: PageProps) => {
-	const { pageID } = await params;
+const Page = () => {
 	return (
 		<div>
-			<h1>{pageID}</h1>
+			<h1>Page</h1>
 		</div>
 	);
 };
