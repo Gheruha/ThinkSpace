648b6390eab9c078068e38f642d3abb42930820e
Curnic Dorin <157069618+C-Dorin@users.noreply.github.com>
2025-02-13 19:30:36 +0200

    Merge errors solved

--- src/app/auth/components/authentication/OAuth.tsx
+++ src/app/auth/components/authentication/OAuth.tsx
@@ -1,22 +1,25 @@
 'use client';
-// File that will hold all the OAuth buttons
+
 import { ThemeImage } from '@/components/themeImage';
 import { Button } from '@/components/ui/button';
+import { handleSignInWithOAuth } from '../handleFunctions';
 import React from 'react';
 
 export const OAuth = () => {
 	return (
 		<div className="w-full flex justify-center pt-10">
-			<form action="api/auth/googleOAuth" method="POST">
-				<Button variant="outline" className="border rounded-full w-16 h-16">
-					<ThemeImage
-						lightSrc="/icons/google.png"
-						darkSrc="/icons/google.png"
-						alt="icon"
-						className="w-8 h-8"
-					/>
-				</Button>
-			</form>
+			<Button
+				variant="outline"
+				className="border rounded-full w-16 h-16"
+				onClick={handleSignInWithOAuth}
+			>
+				<ThemeImage
+					lightSrc="/icons/google.png"
+					darkSrc="/icons/google.png"
+					alt="icon"
+					className="w-8 h-8"
+				/>
+			</Button>
 		</div>
 	);
 };
