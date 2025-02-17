5c62d9e400526e0d4a25b4e1f5526a6dcaca64ee
Gheruha <maxgheruha@gmail.com>
2025-02-17 12:01:43 +0200

    Refactor: moved workspace components

--- src/app/workspace/[pageID]/page.tsx
+++ src/app/workspace/[pageID]/page.tsx
@@ -1,4 +1,4 @@
-import NewPageForm from '@/app/api/workspace/components/newPageForm';
+import NewPageForm from '@/app/workspace/components/newPageForm';
 
 export default async function Page({ params }: { params: Promise<{ pageID: string }> }) {
 	const { pageID } = await params;
