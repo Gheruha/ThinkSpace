68145b547c702b2d0e66926b653f14ac7e8c137f
Curnic Dorin <157069618+C-Dorin@users.noreply.github.com>
2025-02-26 15:05:54 +0200

    Added all types for pricing files

--- src/app/pricing/layout.tsx
+++ src/app/pricing/layout.tsx
@@ -1,5 +1,11 @@
+'use client';
+
 import LearningProductLayout from '@/lib/layouts/marketing';
 
-export default function PricingLayout({ children }: { children: React.ReactNode }) {
+interface PricingLayoutProps {
+	children: React.ReactNode;
+}
+
+export default function PricingLayout({ children }: PricingLayoutProps) {
 	return <LearningProductLayout>{children}</LearningProductLayout>;
 }
